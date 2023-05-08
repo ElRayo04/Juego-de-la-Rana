d2bb6f2ffddfea8ba5d0333755247d3a7bdd87d9
ElRayo04 <hugoelrayo04@gmail.com>
2023-05-08 22:38:02 +0200

    no se como puto hacer el add force
    alluda

--- Assets/Scripts/Enemigos/FireBallEnemy.cs
+++ Assets/Scripts/Enemigos/FireBallEnemy.cs
@@ -10,11 +10,14 @@ public class FireBallEnemy : MonoBehaviour
 
     private Rigidbody2D theRB;    
     private float push = 100f;
+    //Fuerza de empuje del Bouncer
+    public float bounceForce = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
         seePlayer = false;
+        //Inicializamos el rigidbody
         theRB = gameObject.AddComponent<Rigidbody2D>();
     }
 
@@ -29,7 +32,7 @@ public class FireBallEnemy : MonoBehaviour
         //theRB.AddForce(transform.up * push, ForceMode2D.Impulse);
         if (seePlayer == true)
         {
-            AceleronIzquierdo();
+            Bounce(bounceForce);
         }
     }
 
@@ -37,6 +40,12 @@ public class FireBallEnemy : MonoBehaviour
     {
         // Alternatively, specify the force mode, which is ForceMode2D.Force by default
         theRB.AddForce(-transform.forward * push, ForceMode2D.Impulse);
+    }
+
+    public void Bounce(float bounceForce)
+    {
+        //Impulsamos al jugador rebotando
+        theRB.velocity = new Vector2(theRB.velocity.x, bounceForce);
     }
 
 }
